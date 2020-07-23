bce814911511dc72ba1e5c57dcd5c7023fdd4ca6
Jessela26 <jelbaniqued26@gmail.com>
2020-07-23 18:47:01 -0400

    added shopping cart

--- Startup.cs
+++ Startup.cs
@@ -29,6 +29,10 @@ namespace PieShop
             options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddScoped<IPieRepository, PieRepository>();
             services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<ShoppingCart>(sp => ShoppingCart.GetCart(sp));
+            services.AddHttpContextAccessor();
+            services.AddSession();
+            
             services.AddControllersWithViews();
         }
 
@@ -42,6 +46,7 @@ namespace PieShop
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseSession();
 
             app.UseRouting();
 
