40b93645ddba30c81a3df5ae4ef2824d4b2a7bfa
Jessela26 <jelbaniqued26@gmail.com>
2020-07-23 21:17:09 -0400

    added the pie category options

--- Controllers/PieController.cs
+++ Controllers/PieController.cs
@@ -19,14 +19,14 @@ namespace PieShop.Controllers
             _categoryRepository = categoryRepository;
         }
 
-        public ViewResult List()
-        {
-            PiesListViewModel piesListViewModel = new PiesListViewModel();
-            piesListViewModel.Pies = _pieRepository.AllPies;
-
-            piesListViewModel.CurrentCategory = "Cheese Cakes";
-            return View(piesListViewModel);
-        }
+        //public ViewResult List()
+        //{
+        //    PiesListViewModel piesListViewModel = new PiesListViewModel();
+        //    piesListViewModel.Pies = _pieRepository.AllPies;
+        //
+        //    piesListViewModel.CurrentCategory = "Cheese Cakes";
+        //    return View(piesListViewModel);
+        //}
 
         public IActionResult Details(int id)
         {
@@ -34,6 +34,30 @@ namespace PieShop.Controllers
             if(pie == null)
                 return NotFound();
             return View(pie);
+        }
+
+        public ViewResult List(string category)
+        {
+            IEnumerable<Pie> pies;
+            string currentCategory;
+
+            if (string.IsNullOrEmpty(category))
+            {
+                pies = _pieRepository.AllPies.OrderBy(p => p.PieId);
+                currentCategory = "All pies";
+            }
+            else
+            {
+                pies = _pieRepository.AllPies.Where(p => p.Category.CategoryName == category)
+                    .OrderBy(p => p.PieId);
+                currentCategory = _categoryRepository.AllCategories.FirstOrDefault(c => c.CategoryName == category)?.CategoryName;
+            }
+
+            return View(new PiesListViewModel
+            {
+                Pies = pies,
+                CurrentCategory = currentCategory
+            });
         }
     }
 }
